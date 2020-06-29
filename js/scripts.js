42088963883ee2ae7f6f6a3c3e96cc98677b87b3
Kel <kelstalker@hotmail.com>
2020-06-29 11:29:50 -0600

    return to top feature implemented completely now with no bugs

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
-// Back To Top Arrow Method - 
+// NAME: BACK TO TOP  
 $(document).ready(function(){
-	$(window).scroll(function () {
+	     {
 			if ($(this).scrollTop() > 50) {
 				$('#back-to-top').fadeIn();
 			} else {
@@ -16,19 +16,4 @@ $(document).ready(function(){
 		});
 });
 
-// Shorthand for $( document ).ready()
 
-Posted in: Using jQuery Core
-$( document ).ready()
-
-// A page can't be manipulated safely until the document is "ready." jQuery detects this state of readiness for you. Code included inside $( document ).ready() will only run once the page Document Object Model (DOM) is ready for JavaScript code to execute. Code included inside $( window ).on( "load", function() { ... }) will run once the entire page (images or iframes), not just the DOM, is ready.
-
-// A $( document ).ready() block.
-$( document ).ready(function() {
-    console.log( "ready!" );
-});
-
-// Experienced developers sometimes use the shorthand $() for $( document ).ready(). If you are writing code that people who aren't experienced with jQuery may see, it's best to use the long form.
-$(function() {
-    console.log( "ready!" );
-});
