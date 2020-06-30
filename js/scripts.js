98735369da86502b32a94628caf80689d9eaefdf
Kel <kelstalker@hotmail.com>
2020-06-29 20:37:36 -0600

    test2.html is working fully except for nav css

--- js/scripts.js
+++ js/scripts.js
@@ -18,3 +18,24 @@ $(document).ready(function () {
     return false;
   });
 });
+
+// Source: https://html-online.com/articles/dynamic-scroll-back-top-page-button-javascript/
+
+/*Scroll to top when arrow up clicked BEGIN*/
+$(window).scroll(function() {
+  var height = $(window).scrollTop();
+  if (height > 100) {
+      $('#back2Top').fadeIn();
+  } else {
+      $('#back2Top').fadeOut();
+  }
+});
+$(document).ready(function() {
+  $("#back2Top").click(function(event) {
+      event.preventDefault();
+      $("html, body").animate({ scrollTop: 0 }, "slow");
+      return false;
+  });
+
+});
+/*Scroll to top when arrow up clicked END*/
