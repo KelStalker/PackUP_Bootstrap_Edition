a7126f52d16f5d016815c898a35443535a61f062
Kel <kelstalker@hotmail.com>
2020-07-03 10:00:06 -0600

    formatted hamburger to fit better and reduced font size

--- register.php
+++ register.php
@@ -18,14 +18,4 @@ $first_name = mysqli_real_escape_string($link, $_REQUEST['first_name']);
 $last_name = mysqli_real_escape_string($link, $_REQUEST['last_name']);
 $email = mysqli_real_escape_string($link, $_REQUEST['email']);
  
-// Attempt insert query execution
-$sql = "INSERT INTO register (fname, lname, email) VALUES ('$first_name', '$last_name', '$email')";
-if(mysqli_query($link, $sql)){
-    echo "Records added successfully.";
-} else{
-    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
-}
  
-// Close connection
-mysqli_close($link);
-?>
