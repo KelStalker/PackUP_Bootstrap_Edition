940371018d6f6e493f733a2cabfa5e3118b593dd
Kel <kelstalker@hotmail.com>
2020-07-14 09:15:16 -0600

    All changes as of Tue morning

--- js/scripts.js
+++ js/scripts.js
@@ -37,6 +37,7 @@ $(document).ready(function () {
     return false;
   });
 });
+
 /*Scroll to top when arrow up clicked END*/
 
 
@@ -47,30 +48,31 @@ $(document).ready(function () {
 //     *****************((*******************
 
 // Get the modal
-  var modal = document.getElementById("myModal");
+var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
-  var btn = document.getElementById("myBtn");
+var btn = document.getElementById("myBtn");
 
 // Get the <span> element that closes the modal
-  var span = document.getElementsByClassName("close")[0];
+var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal
-  btn.onclick = function () {
-    modal.style.display = "block";
-  };
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
 
 // When the user clicks on <span> (x), close the modal
-  span.onclick = function () {
-    modal.style.display = "none";
-  };
+span.onclick = function() {
+  modal.style.display = "none";
+}
 
 // When the user clicks anywhere outside of the modal, close it
-  window.onclick = function (event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
-  };
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
 
 //     ************************************
 //     *****     End Modal Script     *****
